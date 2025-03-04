0a73fb3947830b335de689a68206335ddba4bc70
jchen <hchen@esri.com>
2025-03-03 20:11:39 -0800

    chore: initial commit

---
+++ charts-components-with-map-components-react-demo/src/index.jsx
@@ -0,0 +1,54 @@
+import { StrictMode, useRef, useEffect } from "react";
+import { createRoot } from "react-dom/client";
+import { createFeatureLayer } from "./functions/create-feature-layer";
+
+import "@arcgis/map-components/components/arcgis-map";
+import { ScatterPlotModel } from "@arcgis/charts-model";
+import { defineCustomElements as defineCalciteElements } from "@esri/calcite-components/dist/loader";
+import { defineCustomElements as defineChartsElements } from "@arcgis/charts-components/dist/loader";
+
+import "./index.css";
+
+// define custom elements in the browser, and load the assets from the CDN
+defineCalciteElements();
+defineChartsElements(window, {
+  resourcesUrl: "https://js.arcgis.com/charts-components/4.32/assets",
+});
+
+const App = () => {
+  const chartRef = useRef(null);
+
+  useEffect(() => {
+    const initializeChart = async () => {
+      const layer = await createFeatureLayer(
+        "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/ArcGIS/rest/services/ChicagoCr/FeatureServer/0"
+      );
+
+      const scatterplotModel = new ScatterPlotModel();
+      await scatterplotModel.setup({ layer });
+
+      await scatterplotModel.setXAxisField("Ward");
+      await scatterplotModel.setYAxisField("Beat");
+
+      const config = scatterplotModel.getConfig();
+
+      if (chartRef.current) {
+        chartRef.current.layer = layer;
+        chartRef.current.model = config;
+      }
+    };
+
+    initializeChart();
+  }, []);
+
+  return (
+    <StrictMode>
+      <arcgis-chart ref={chartRef}>
+        <arcgis-charts-action-bar slot="action-bar"></arcgis-charts-action-bar>
+      </arcgis-chart>
+    </StrictMode>
+  );
+};
+
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
